4faec0cb401551878ef08f3304646b19756404de
IOT / WEB DESIGN <92590229+HamzaAbbas0@users.noreply.github.com>
2022-08-24 03:02:07 +0500

    Add files via upload

---
+++ Assigment-1.py
@@ -0,0 +1,8 @@
+def function():
+    name=input("Enter your Name")
+    income=int(input("Enter you Monthly Incomeme"))
+    income =income *12
+    print(f"{name} your Anual Income is {income}")
+
+
+function() 
